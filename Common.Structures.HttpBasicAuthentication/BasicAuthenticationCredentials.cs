a6661059aa174e0d79d79e397747d550c94014e5
Christopher Dorst <chris@dorst.email>
2018-01-03 03:22:54 -0600

    Add BasicAuthenticationCredentials

---
+++ Common.Structures.HttpBasicAuthentication/BasicAuthenticationCredentials.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+using static System.Convert;
+using static System.Text.Encoding;
+
+namespace Common.Structures.HttpBasicAuthentication
+{
+    public struct BasicAuthenticationCredentials
+    {
+        private const string Basic = nameof(Basic);
+
+        public BasicAuthenticationCredentials(string user, string secret)
+        {
+            User = user;
+            Secret = secret;
+        }
+
+        public string User { get; set; }
+        public string Secret { get; set; }
+
+        public AuthenticationHeaderValue Header
+            => new AuthenticationHeaderValue(Basic, HeaderValue);
+
+        private string HeaderValue
+            => ToBase64String(UTF8.GetBytes($"{User}:{Secret}"));
+    }
+}
