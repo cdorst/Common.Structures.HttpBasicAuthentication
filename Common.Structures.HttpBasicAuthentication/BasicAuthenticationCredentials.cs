712b62ad043990cb05d5ae854051d860b672704b
Christopher Dorst <chris@dorst.email>
2018-01-13 22:56:02 -0600

    Update HttpAuthentication reference

--- Common.Structures.HttpBasicAuthentication/BasicAuthenticationCredentials.cs
+++ Common.Structures.HttpBasicAuthentication/BasicAuthenticationCredentials.cs
@@ -1,14 +1,13 @@
-﻿using static System.Convert;
+﻿using Common.Structures.HttpAuthentication;
+using static System.Convert;
 using static System.Text.Encoding;
 
 namespace Common.Structures.HttpBasicAuthentication
 {
     public class BasicAuthenticationCredentials : HttpAuthentication.HttpAuthentication
     {
-        private const string Basic = nameof(Basic);
-
         public BasicAuthenticationCredentials(string user, string secret)
-            : base(Basic, ToBase64String(UTF8.GetBytes($"{user}:{secret}")))
+            : base(PrefixType.Basic, ToBase64String(UTF8.GetBytes($"{user}:{secret}")))
         {
             User = user;
         }
